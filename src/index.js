943b2d239ac12043e0adc67e160c48aac8cc4ffc
4drianC4 <camararequeadrianrafael@gmail.com>
2024-06-09 23:49:16 -0400

    finalizado

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createServer } from "node:http";
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
+app.use(express.json());
 
 const server = createServer(app);
 const io = new Server(server);
@@ -23,17 +24,45 @@ io.on("connection", (socket) => {
 });
 
 const midleware = (req, res, next) => {
-  const { login } = req.query;
-  if (login === "true") {
-    next();
-  } else {
-    res.sendFile(process.cwd() + "/client/login.html");
+  try {
+  const users = 
+    [{"username": "admin", "password": "admin"},
+    {"username": "user1", "password": "user1"},
+    {"username": "user2", "password": "user2"}];
+  const { username, password } = req.body;
+  console.log(username, password);
+  users.map((user) => {
+  if (user.username === username && user.password === password) {
+    console.log("Login successful");
+    return next();
+  }else{
+    return res.status(400).json({message: "Login failed"});
   }
+  });
+} catch (error) {
+  return res.status(400).json({message: "Login failed", error: error});}
 };
 
-app.get("/", midleware, (req, res) => {
-  res.sendFile(process.cwd() + "/client/index.html");
+app.get("/",(req, res) => {
+  res.sendFile(process.cwd() + "/client/login.html");
 });
+
+app.post("/login", (req, res) => {
+  const users = 
+    [{"username": "admin", "password": "admin"},
+    {"username": "user1", "password": "user1"},
+    {"username": "user2", "password": "user2"}];
+  const { username, password } = req.body;
+  console.log(username, password);
+  users.map((user) => {
+  if (user.username === username && user.password === password) {
+    console.log("Login successful");
+    return res.status(200).json({message: "Login successful", data: user});
+  }
+  });
+  return res.status(400).json({message: "Login failed"});
+}
+);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
